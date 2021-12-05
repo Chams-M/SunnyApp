4a373c803e55e4ec09494dc182124eb10f12db80
Chams <57327689+Chams-M@users.noreply.github.com>
2021-12-05 13:23:03 +0100

    Update validator.dart

--- lib/classes/validator.dart
+++ lib/classes/validator.dart
@@ -2,7 +2,7 @@
 class Validator {
   static String? validateName({required String name}) {
     if (name == null) {
-      return null;
+      return null; 
     }
     if (name.isEmpty) {
       return 'Name can\'t be empty';
