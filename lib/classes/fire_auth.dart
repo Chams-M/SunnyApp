5686e621205b134eeb648d59f4608c5eada9d4ce
Chams <57327689+Chams-M@users.noreply.github.com>
2021-12-05 13:22:18 +0100

    Update fire_auth.dart

--- lib/classes/fire_auth.dart
+++ lib/classes/fire_auth.dart
@@ -1,7 +1,7 @@
 //This class is for manipulating Firebase authentication
 // it contains 2 methods for Registration and authentication with Firebase
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; 
 
 class FireAuth {
   static Future<User?> registerUsingEmailPassword({
